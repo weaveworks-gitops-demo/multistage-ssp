8c18ec17d2106f2f569ed82e7308df1c5f5d0988
Leonardo Murillo <leonardo@weave.works>
2021-09-13 21:25:52 +0000

    Simplifying Stack

--- terraform/ssp/main.tf
+++ terraform/ssp/main.tf
@@ -17,11 +17,9 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = var.availability_zones
-  private_subnets = var.private_subnet_cidrs
   public_subnets  = var.public_subnet_cidrs
 
-  enable_nat_gateway = true
-  one_nat_gateway_per_az = true
+  enable_nat_gateway = false
 
   tags = {
     Terraform = "true"
