ac68b813ee989325a96947d7683822d400f9fb4f
Leonardo Murillo <leonardo@weave.works>
2021-09-13 15:23:37 +0000

    Quick fix to startup script

--- start.sh
+++ start.sh
@@ -28,8 +28,8 @@ echo ""
 TERRAFORM_VER=`curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |  grep tag_name | cut -d: -f2 | tr -d \"\,\v | awk '{$1=$1};1'`
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VER}/terraform_${TERRAFORM_VER}_linux_amd64.zip
 sudo yum -y install unzip
-unzip terraform_${TERRAFORM_VER}_linux_amd64.zip
-sudo mv terraform /usr/local/bin/terraform
+unzip terraform_${TERRAFORM_VER}_linux_amd64.zip -d /tmp
+sudo mv /tmp/terraform /usr/local/bin/terraform
 rm -rf terraform_${TERRAFORM_VER}_linux_amd64.zip
 
 echo ""
